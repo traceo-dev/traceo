928f16580f05a0e53319b2fd2a16c478757818cd
Piotrek Szewczyk <p.szewczyk@doittechnology.pl>
2022-09-04 14:52:03 +0200

    fix/esilnt fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin', '@typescript-eslint'],
+  plugins: ['@typescript-eslint'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
