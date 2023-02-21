d096d00aba162f8503c4ff84aa11ae068ec51c93
Piotrek <p.szewczyk-98@wp.pl>
2023-02-21 21:41:12 +0100

    fix/improvements to login page

--- public/packages/app/src/core/components/Layout/AuthLayout.tsx
+++ public/packages/app/src/core/components/Layout/AuthLayout.tsx
@@ -1,4 +1,3 @@
-import { VERSION } from "../../utils/constants";
 import { TraceoLogo } from "../Icons/TraceoLogo";
 import { Space, Typography } from "@traceo/ui";
 import { FC } from "react";
@@ -13,11 +12,13 @@ export const AuthLayout: FC<Props> = ({ children, title }) => {
     <div className="w-full grid grid-cols-2">
       <div className="col-span-1">
         <div className="flex h-screen items-center justify-center flex-col">
-          <TraceoLogo size="medium" />
-          <Typography size="xxl" weight="semibold" className="mb-5 mt-2">
-            Welcome to Traceo
-          </Typography>
-          <Typography size="xs">v.{VERSION}</Typography>
+          <div className="flex flex-row">
+            <TraceoLogo size="medium" />
+            <div className="flex flex-col text-primary pl-3">
+              <span className="text-[24px] font-semibold">Traceo</span>
+              <span className="text-xs">Take control of your applications</span>
+            </div>
+          </div>
         </div>
       </div>
       <div className="col-span-1 bg-primary">
@@ -25,7 +26,7 @@ export const AuthLayout: FC<Props> = ({ children, title }) => {
           <div className="text-white p-12 max-w-md w-full">
             <Space direction="vertical" className="pb-12">
               {title && <span className="font-semibold text-3xl text-primary">{title}</span>}
-              <Typography>Please enter your details.</Typography>
+              <Typography>Please log in to your account.</Typography>
             </Space>
             {children}
           </div>
