233ee347600cd0a9ab0ef792b1effc94e68c341a
Piotrek <p.szewczyk-98@wp.pl>
2023-02-21 21:24:25 +0100

    fix/login page improvements

--- public/packages/app/src/core/components/Layout/AuthLayout.tsx
+++ public/packages/app/src/core/components/Layout/AuthLayout.tsx
@@ -11,20 +11,21 @@ interface Props {
 export const AuthLayout: FC<Props> = ({ children, title }) => {
   return (
     <div className="w-full grid grid-cols-2">
-      <div className="col-span-1"></div>
+      <div className="col-span-1">
+        <div className="flex h-screen items-center justify-center flex-col">
+          <TraceoLogo size="medium" />
+          <Typography size="xxl" weight="semibold" className="mb-5 mt-2">
+            Welcome to Traceo
+          </Typography>
+          <Typography size="xs">v.{VERSION}</Typography>
+        </div>
+      </div>
       <div className="col-span-1 bg-primary">
         <div className="flex h-screen justify-center items-center">
           <div className="text-white p-12 max-w-md w-full">
-            <Space direction="vertical" className="justify-center w-full items-center pb-12">
-              <TraceoLogo size="medium" />
-
-              {title && (
-                <Typography size="xxl" weight="semibold">
-                  {title}
-                </Typography>
-              )}
-
-              <Typography size="xs">v.{VERSION}</Typography>
+            <Space direction="vertical" className="pb-12">
+              {title && <span className="font-semibold text-3xl text-primary">{title}</span>}
+              <Typography>Please enter your details.</Typography>
             </Space>
             {children}
           </div>
