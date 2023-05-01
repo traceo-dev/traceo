bedfab7e8a96b38663608293bdbf44b6a2567735
Piotrek <p.szewczyk-98@wp.pl>
2023-05-01 14:15:34 +0200

    fix/page previe header text leading fix

--- public/packages/app/src/core/components/PreviewPageHeader.tsx
+++ public/packages/app/src/core/components/PreviewPageHeader.tsx
@@ -9,7 +9,7 @@ export const PreviewPageHeader = ({ description, page, title }: Props) => {
   return (
     <Space direction="vertical" className="gap-0 w-full">
       <span className="text-[9px] uppercase">{page}</span>
-      <span className="text-white text-3xl font-semibold leading-5">{title}</span>
+      <span className="text-white text-3xl font-semibold leading-7">{title}</span>
       {typeof description === "string" ? (
         <Typography className="pt-2">{description}</Typography>
       ) : (
